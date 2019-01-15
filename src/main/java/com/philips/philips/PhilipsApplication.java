387e19a5794fa2b4bb18345877e7fec033296157
Raphael Luiz Martins Koszalka <raphael.koszalka@philips.com>
2019-01-15 20:40:04 -0200

    intial commit

---
+++ src/main/java/com/philips/philips/PhilipsApplication.java
@@ -0,0 +1,14 @@
+package com.philips.philips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilipsApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/philips/philips/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.philips.philips.controller;
+
+public class UserController {
+}
