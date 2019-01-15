411114849ca6a278889f79bf7313d9eec4b4dbf9
Raphael Luiz Martins Koszalka <raphael.koszalka@philips.com>
2019-01-15 21:10:33 -0200

    crud working

--- src/main/java/com/philips/philips/controller/UserController.java
+++ src/main/java/com/philips/philips/controller/UserController.java
@@ -1,4 +1,48 @@
 package com.philips.philips.controller;
 
+import com.philips.philips.exception.ResourceNotFoundException;
+import com.philips.philips.model.User;
+import com.philips.philips.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public Page<User> getQuestions(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @PostMapping("/users")
+    public User createQuestion(@Valid @RequestBody User question) {
+        return userRepository.save(question);
+    }
+
+    @PutMapping("/users/{userId}")
+    public User updateQuestion(@PathVariable Long userId,@Valid @RequestBody User userRequest) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    user.setName(userRequest.getName());
+                    user.setSurname(userRequest.getSurname());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + userId));
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<?> deleteQuestion(@PathVariable Long userId) {
+        return userRepository.findById(userId)
+                .map(question -> {
+                    userRepository.delete(question);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + userId));
+    }
 }
---
+++ src/main/java/com/philips/philips/model/User.java
@@ -0,0 +1,50 @@
+package com.philips.philips.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users_two")
+public class User {
+    @Id
+    @GeneratedValue(generator = "users_generator")
+    @SequenceGenerator(
+            name = "users_generator",
+            sequenceName = "users_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String surname;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
